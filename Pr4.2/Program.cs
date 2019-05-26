acaef38d9be5997f86a6be25e5b541dac8a1e94b
KozlovaDasha <dashakozlova255@ukr.net>
2019-05-26 23:02:20 +0300

    Добавьте файлы проекта.

---
+++ Pr4.2/Program.cs
@@ -0,0 +1,42 @@
+﻿//Заміна рядків
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pr4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputText;
+            char[] separator = { ' ', '.', '*', '+', ';', ',', '?', '!', '‐', '/' };
+            string otvet;
+
+            using (StreamReader MyFile = new StreamReader("text.txt"))
+            {
+                
+                inputText = MyFile.ReadToEnd().Replace('\n', ' ');
+            }
+            string[] words = inputText.Split(separator);
+
+          
+                    otvet = inputText.Replace("Work", "Job");
+                    Console.WriteLine(otvet);
+            Console.WriteLine("------------------");
+            for(int i=0; i<words.Length;i++)
+            {
+                if (words[i] == "life")
+                { Console.WriteLine("Index:" + i); break; }
+            }
+            Console.ReadLine();
+          
+
+        }
+
+      
+    }
+}
